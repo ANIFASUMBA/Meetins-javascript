0d1801e7abb25296262a479bf192a60b3eb4d038
ANIFA SUMBA <sumbaanifa@gmail.com>
2024-11-11 13:46:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String []args){
+        int length;
+        int innerWidth;
+        System.out.println("Enter value for width: ");
+        System.out.println("Enter value for width: ");
+        area= length *width;
+        System.out.println("The area of the rectangle is" +area);
+        
+    }
+}
+
